b55f471870b4f19666fcccaf3c5a87305520f67d
ihtesham ul haq <ihtesham.ulhaq4959@gmail.com>
2023-09-13 22:20:44 -0700

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res)=>{
-    res.end('my name is pamualdeep Kaur Mander and my student number is 160357216');
+    res.end('<h1>My name is Pamualdeep Kaur Mander and my student number is 160357216<h1/>');
 });
 const port = 5000;
 server.listen(5000);
-console.log('server is working ${5000}')
+console.log('server is working ${`port`}')
