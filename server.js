4bd59e606b0b35bab2aa742bf30fb6af3416d8f0
ihtesham ul haq <ihtesham.ulhaq4959@gmail.com>
2023-09-13 22:37:32 -0700

    commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res)=>{
-    res.end('<h1>My name is Pamualdeep Kaur Mander and my student number is 160357216<h1/>');
+    res.end('My name is Pamualdeep Kaur Mander and my student number is 160357216');
 });
 const port = 5000;
 server.listen(5000);
